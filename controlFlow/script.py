025a739d4a5bd26c7b88a70a066be68e14796f1f
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-23 13:45:48 -0400

    more ways to create boolean expressions #9

--- controlFlow/script.py
+++ controlFlow/script.py
@@ -1,13 +1,14 @@
-#Check the type
-
-my_baby_bool = "true"
-
-print(type(my_baby_bool))
-
-my_baby_bool_two = True
-
-print(type(my_baby_bool_two))
-
-
-
-
+#This collegue requires 120 credits to graduate
+def greater_than(x, y):
+  if x > y:
+    return x
+  if y > x:
+    return y
+  if x == y:
+    return "These numbers are the same"
+    
+def graduation_reqs(credits):
+  if credits >= 120:
+    return "You have enough credits to graduate!"
+  
+print(graduation_reqs(120))
