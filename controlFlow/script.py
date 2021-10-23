a5c749b082c93445f0f89473f26de9d2000e5258
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-23 12:15:50 -0400

    Bool? #2

---
+++ controlFlow/script.py
@@ -0,0 +1,16 @@
+#Boolean or not
+
+#My dog is the cutest dog in the world
+example_statement = "No"
+
+#Dogs are mammals
+statement_one = "Yes"
+
+#My dog is named Pavel
+statement_two = "Yes"
+
+#Dogs make the best pets
+statement_three = "No"
+
+#Cats are female dogs
+statement_four = "Yes"
