7f287b0d182b5a737d4205c863822130748be987
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-24 19:10:26 -0400

    def shipping_cost_drone #31

--- shippingCost/script.py
+++ shippingCost/script.py
@@ -15,3 +15,18 @@ print(shipping_cost_ground(8.4))
 
 shipping_cost_premiun = 125.00
 
+def shipping_cost_drone(weight):
+  
+  if weight <= 2:
+    price_per_pound = 4.50
+  elif weight <= 6:
+    price_per_pound = 9.00
+  elif weight <= 10:
+    price_per_pound = 12.00
+  else:
+    price_per_pound = 14.25
+
+  return price_per_pound * weight
+
+print(shipping_cost_drone(1.5))
+
