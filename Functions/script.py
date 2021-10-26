708f1c115f3b6874d6835f656cc221a30f556827
oldoc63 <leopoldo.olmos@gmail.com>
2021-10-26 18:19:46 -0400

    Expenses of my trip #47

--- Functions/script.py
+++ Functions/script.py
@@ -1,8 +1,8 @@
-def directions_to_timesSq():
-  print("Walk 4 mins to 34th St Herald Square train station.")
-  print("Take the Northbound N, Q, R, or W train 1 stop.")
-  print("Get off the Times Square 42nd Street stop.")
-  print("Take lots of pictures!")
+# Write your code below: 
+def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_time):
+  car_rental_total = car_rental_rate * trip_time
+  hotel_total = hotel_rate * trip_time - 10
+  print('The expenses for your trip are goin to be', car_rental_total + hotel_total + plane_ticket_price)
 
-# Call your function here:
-directions_to_timesSq()
+calculate_expenses(200, 100, 100, 5)
+
