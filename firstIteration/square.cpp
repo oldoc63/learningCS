736b39d73cdd03ba7a87abc886666e12bbb36046
oldoc63 <leopoldo.olmos@gmail.com>
2021-11-16 12:59:31 -0400

    first program ever to run on a stored-program computer #49

---
+++ firstIteration/square.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int i = 0;
+	int square = 0;
+
+	while(i < 10){
+		square = i * i;
+		cout << i << " " << square << "\n";
+		i++;
+	}
+}
