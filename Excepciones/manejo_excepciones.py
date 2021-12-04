ef60b521721cb9a6f8960d45fde8c0ae956b47f3
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-04 18:43:55 -0400

    Ejemplo de manejo de una excepción con try/except #57

---
+++ Excepciones/manejo_excepciones.py
@@ -0,0 +1,11 @@
+resultado = None
+a = 10
+b = 0
+
+try:
+    resultado = a/b
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+
+print(f'Resultado: {resultado}')
+print('Continuamos ... ')
