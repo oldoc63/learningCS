c84b86895dbdca041412f0cb7db418dc97aebead
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-24 14:22:25 -0400

    Agregar acentos using encoding #72

--- archivos/manejo_archivos.py
+++ archivos/manejo_archivos.py
@@ -1,7 +1,10 @@
 try:
-    archivo = open('prueba.txt','w')
+    archivo = open('prueba.txt','w', enconding='utf8')
+    archivo.write('Agregamos información al archivo\n')
+    archivo.write('Adios')
 except Exception as e:
     print(e)
 finally:
     archivo.close()
-    
+    print('Fin del archivo')
+
