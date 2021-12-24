5156e90c3f3cb4aac149356054dd6970c851f43f
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-24 18:23:38 -0400

    Copiar o anexar informacion en otro archivo #72

--- archivos/leer_archivo.py
+++ archivos/leer_archivo.py
@@ -2,5 +2,20 @@ archivo = open('prueba.txt', 'r', encoding='utf8')
 #print(archivo.read())
 #print(archivo.read(5))
 #print(archivo.read(3))
-print(archivo.readline())
-print(archivo.readline())
+# print(archivo.readline())
+# print(archivo.readline())
+
+#iterar el archivo
+# for linea in archivo:
+#     print(linea)
+
+#leer lineas
+# print(archivo.readlines()) #Retorna una lista
+# print(archivo.readlines()[0])
+
+#a - anexar información
+archivo2 = open('copia.txt', 'a')
+archivo2.write(archivo.read())
+
+archivo.close()
+archivo2.close()
