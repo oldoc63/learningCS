77a7b2d01f6c74abbd01f3c0b7a0b6e4b3bc67b5
oldoc63 <leopoldo.olmos@gmail.com>
2021-12-28 18:49:38 -0400

    Retrieve data from a table #88

---
+++ DB/pyapp/retrieveFromTable.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+try:
+    conexion = psycopg2.connect(
+        user='oldoc',
+        password='leodoc754',
+        host='127.0.0.1',
+        port='5432',
+        database='pyapp'
+)
+    print("connected to the database")
+except:
+    print("unable to connect to the database")
+
+print(conexion)
+
+cursor = conexion.cursor()
+
+try:
+    sentencia = '''
+   SELECT * FROM weather;
+    '''
+    cursor.execute(sentencia)
+    print('data retrieved')
+except:
+    print('failed to retrieve data')
+
+registros = cursor.fetchall()
+print(registros)
+
+cursor.close()
+conexion.close()
