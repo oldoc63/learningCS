2e72417a3f6a183f1ce0e4961da9e4f83f10ec58
oldoc63 <leopoldo.olmos@gmail.com>
2022-01-02 13:23:57 -0400

    Two sentences one transaction: all or none #134

--- DB/pyapp/transacciones.py
+++ DB/pyapp/transacciones.py
@@ -18,10 +18,15 @@ try:
     conexion.autocommit = False
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
-    valores = ('Karla', 'Gomez','kgomez@mail.com')
+    valores = ('Carlos', 'Lara','clara@mail.com')
     cursor.execute(sentencia, valores)
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan Carlos', 'Juarez', 'jcjuarez@mail.com', 1)
+    cursor.execute(sentencia,valores)
+    
     conexion.commit()
-    print('Termina la transaccion')
+    print('Termina la transaccion, se hizo commit')
 except Exception as e:
     conexion.rollback()
     print(f'Ocurrió un error, se hizo rollback {e}')
