14413a8e58fcbdac5584b4e478c96e8ee1a087a5
oldoc63 <leopoldo.olmos@gmail.com>
2022-01-02 08:12:43 -0400

    city in weather references cities(name) #130

--- DB/pyapp/create_table.py
+++ DB/pyapp/create_table.py
@@ -18,9 +18,12 @@ cursor = conexion.cursor()
 
 try:
     sentencia = '''
-    CREATE TABLE cities (
-        name     varchar(80) primary key,
-        location point
+    CREATE TABLE weather (
+        city      varchar(80) references cities(name),
+        temp_lo   int,
+        temp_hi   int,
+        prcp      real,
+        date      date
 );
     '''
     cursor.execute(sentencia)
