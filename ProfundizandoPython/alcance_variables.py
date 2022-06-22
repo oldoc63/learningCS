47bace6fea975b5727bb8305533d97ff375e272d
oldoc63 <leopoldo.olmos@gmail.com>
2022-06-22 15:38:49 -0400

    Variable Global y local #223

---
+++ ProfundizandoPython/alcance_variables.py
@@ -0,0 +1,20 @@
+# Alcance de Variables (scope)
+
+var_global = 'Variable Global'
+
+def imprimir():
+    #Acceder a una variable global
+    print(f'Variable Global desde función: {var_global}')
+    #Definir variable local
+    var_local = 'variable local'
+    print(f'Variable local desde función: {var_local}')
+    #No se puede acceder a var_local fuera de la función
+    #Pero si desde fucniones anidadas
+    def funcion_anidada():
+        print(f'variable local dentro de función anidada: {var_local}')
+    #Debemos llamar a la función anidada para que se ejecute
+    funcion_anidada()
+
+
+imprimir()
+print(f'Variable Global fuera de la función: {var_global}')
